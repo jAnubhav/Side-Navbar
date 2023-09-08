2d1c690e12c5d2324f712d5cb921fffe043e6eb3
Anubhav Jain <87580533+jAnubhav@users.noreply.github.com>
2023-09-08 14:25:04 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,68 +1,69 @@
-const createNavElement = (e, parent, funcName = null) => {
+const createNavElement = (elem, parent) => {
     let li = document.createElement("li");
 
-    let link = document.createElement("a");
+    const link = navItemTemplate.content.cloneNode(true).children[0];
     link.href = "#";
-    link.title = e[0];
+    link.classList.add((elem.length == 3) ? elem[2] : "link");
 
-    let clsName = (e.length == 3) ? (e[1] == null) ? `link ${e[2]}` : e[2] : "link";
-    link.className = `d-flex nav-item ${clsName}`;
+    let icon = link.querySelector("[data-icon]");
+    let title = link.querySelector("[data-title]");
 
-    let icon = document.createElement("i");
-    icon.classList.add(
-        `fa-${(e.length == 3) ? "solid" : "regular"}`, `fa-${(e[1] == null || e.length == 1) ? e[0].toLowerCase() : e[1]}`
-    );
+    icon.classList.add(`fa-${!([1, 3].includes(elem.length)) ? "regular" : "solid"}`, `fa-${(elem.length == 1) ? elem[0].toLowerCase() : elem[1]}`);
+    link.title = title.textContent = elem[0];
 
-    let p = document.createElement("p");
-    p.className = "link-title";
-    p.innerText = e[0];
-
-    link.append(icon, p);
-    li.append(link);
-
+    li.appendChild(link);
     parent.append(li);
+
+    links.push(link);
 };
 
-const openNavbar = () => {
+let openMenu = () => {
     header.classList.toggle("h-full");
-    navbar.classList.toggle("d-none");
+    navContainer.classList.toggle("d-none");
     setTimeout(() => {
-        navbar.classList.toggle("opacity-0");
-    }, 500);
+        navContainer.classList.toggle("opacity-0");
+    }, 300);
 };
 
-const upperNavElements = [["Home", null, "active"], ["Search"]];
+let closeMenu = () => {
+    if (window.innerWidth <= 750 && header.ariaLabel == "open") {
+        header.classList.remove("h-full");
+        navContainer.classList.add("d-none", "opacity-0");
+        header.ariaLabel = "closed";
+    } else if (window.innerWidth > 750) {
+        header.classList.add("h-full");
+        navContainer.classList.remove("d-none", "opacity-0");
+        header.ariaLabel = "open";
+    }
+};
 
-const mainNavElements = [
-    ["Fashion", "vest"],
-    ["Electronics", "air-conditioner"],
-    ["Home Decor", "lamp-street"],
-    ["Beauty", "wreath-laurel"],
-    ["Groceries", "basket-shopping"],
-    ["Utilities", "kitchen-set"]
+const navElements = [
+    [["Home"], ["Search"]],
+    [
+        ["Fashion", "vest"],
+        ["Electronics", "air-conditioner"],
+        ["Home Decor", "lamp-street"],
+        ["Beauty", "wreath-laurel"],
+        ["Groceries", "basket-shopping"],
+        ["Utilities", "kitchen-set"]
+    ],
+    [["My Address", "location-dot"], ["My Cart", "cart-shopping"], ["Sign In", "user", "profile"]]
 ];
 
-const lowerNavElements = [["My Address", "location-dot"], ["My Cart", "cart-shopping"], ["Sign In", "user", "profile"]];
-
-const navbar = document.querySelector(".nav-container");
-
-const upperNav = document.querySelector(".upper-nav");
-const mainNav = document.querySelector(".main-nav");
-const lowerNav = document.querySelector(".lower-nav");
-
-upperNavElements.forEach(e => { createNavElement(e, upperNav); });
-mainNavElements.forEach(e => { createNavElement(e, mainNav); });
-lowerNavElements.forEach(e => { createNavElement(e, lowerNav); });
+const navItemTemplate = document.getElementById("link");
 
 const header = document.querySelector("header");
+const navContainer = document.querySelector(".nav-container");
 
-setInterval(() => {
-    if (window.innerWidth <= 600 && navbar.ariaLabel == "open") {
-        navbar.classList.add("d-none", "opacity-0");
-        navbar.ariaLabel = "closed";
-    } else if (window.innerWidth > 600) {
-        navbar.classList.remove("d-none", "opacity-0");
-        navbar.ariaLabel = "open";
-        header.classList.remove("h-full")
-    }
-}, 100);
+const navs = Array.from(document.getElementsByClassName("navbar"));
+const links = [];
+
+navs.forEach((nav, index) => {
+    navElements[index].forEach(elem => {
+        createNavElement(elem, nav);
+    });
+});
+
+links[0].classList.add("active");
+
+setInterval(closeMenu, 100);
